e453ca533cc20aca35fef6de09cbc1b7df13c3bd
feverishkk <feverishkk@gmail.com>
2022-10-11 23:16:19 +0900

    Add EF & DbContext

---
+++ Management.Domain/Models/ApplicationUser.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Management.Domain.Models
+{
+    /// <summary>
+    /// DB에 저장되는 모델들
+    /// </summary>
+    public class ApplicationUser : IdentityUser
+    {
+        [NotMapped]
+        [Required]
+        [Compare("Email", ErrorMessage = "Please Write Email Address")]
+        public string ConfirmEmail { get; set; }
+
+        [NotMapped]
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [NotMapped]
+        [Required]
+        [DataType(DataType.Password, ErrorMessage = "Password NOT MATCH!")]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Text, ErrorMessage = "Invalid")]
+        public string GivenName { get; set; }
+        [Required]
+        [DataType(DataType.Text, ErrorMessage = "Invalid")]
+        public string FamilyName { get; set; }
+
+        [Required]
+        public string Department { get; set; }
+
+        [Required]
+        public string DepartmentNumber { get; set; }
+
+        public DateTime? MemberSince { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ ManagementDbContext/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Management.Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementDbContext.DbContext
+{
+    /// <summary>
+    /// DB와 연동하는 Context
+    /// </summary>
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; } = default;
+    }
+}
