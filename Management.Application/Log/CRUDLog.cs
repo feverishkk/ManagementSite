ef253ee59037a2621faf3d5d7025f7b733b9c9ad
feverishkk <feverishkk@gmail.com>
2022-11-03 22:12:05 +0900

    Add Logging List

---
+++ Management.Application/Log/CRUDLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Management.Application.Log
+{
+    public class CRUDLog : Log
+    {
+        /// <summary>
+        /// Create, Delete, Update의 타겟이 되는 대상
+        /// </summary>
+        public object Target { get; set; } = string.Empty;
+    }
+}
---
+++ Management.Application/Log/Log.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Management.Application.Log
+{
+    public class Log
+    {
+        public string Path { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string Method { get; set; }
+        public string UserName { get; set; }
+        public string RemoteIpAddress { get; set; }
+        public string StatusCode { get; set; }
+    }
+}
